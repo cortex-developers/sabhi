4ef997f7523c92f76b86ffdb07e917c10912b24d
Puneet Velidi - p0v01tx <Puneet.Velidi@walmart.com>
2024-02-29 18:13:20 -0600

    scrolling fix

--- src/BioGallery.js
+++ src/BioGallery.js
@@ -171,11 +171,13 @@ function BioGallery() {
     left: '50%',
     transform: 'translate(-50%, -50%)',
     width: '85%', // Adjusted width for better mobile view
-    maxWidth: 375, // Added maxWidth for responsiveness    
+    maxWidth: 375, // Added maxWidth for responsiveness 
+    maxHeight: 500,   
     bgcolor: 'background.paper',
     border: '2px solid #000',
     boxShadow: 24,
     p: 4,
+    overflowY: 'auto'
   };
 
   return (

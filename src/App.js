a64eb8e7bb7a676ab26af6b218342b0589fce4d6
Puneet Velidi <psv23@cornell.edu>
2024-08-26 01:28:25 -0400

    some changes

--- src/App.js
+++ src/App.js
@@ -2,49 +2,13 @@ import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link as RouterLink } from 'react-router-dom';
 import { useMediaQuery, createTheme, ThemeProvider, AppBar, Toolbar, Typography, Button, Box, Container, IconButton, Drawer, List, ListItem, ListItemText} from '@mui/material';
 import { Menu as MenuIcon } from '@mui/icons-material';
-//import downloadapp from './downloadapp.png'
-//import Logo from './sabhi-logo.svg';
-//import Slideshow from './Slideshow';
 import Pricing from './Pricing'
-//import Community from './Community';
 import thecortex from "./thecortex.mp4";
 import thecortexmobile from "./thecortexmobile.mp4";
-import WHAT from './WHAT IS THECORTEX _.png'
-//import PRICING from './pricing.png'
-
-import CR from './CORTEXRESEARCH.png'
-
-//import Article from './Article';
 import LogoNoText from './Group 82.png';
-//import LogoText from './Group 82.png';
 import BioGallery from './BioGallery';
-//import Contact from './Contact';
-//import UnderConstructionPage from './UnderConstructionPage';
-import CortexLearn from './CORTEXLEARN.png';
 import BlogPosts from './BlogPosts';
 import './App.css'
-/* import nateathlete from './nateathleteshot.JPEG'
-import katyathlete from './katyathleteshot.jpg'
-import katyathletedesktop from './katyathleteshotdesktop.jpg'
-import ellaathlete from './ellaathlete.png'
-import ellaathletedesktop from './ellathletedesktop.png'
-import sophiasathlete from './sophiasathlete.jpeg'
-//import brendanathlete from './brendanathleteshot.jpg'
-import oliviathlete from './oliviaathleteshot.png'
-import oliviathletemobile from './oliviaathleteshotmobile.jpeg'
-import jesusathlete from './jesusaction.jpeg'
-import brookeathlete from './brookeathlete.jpg'
-import joeathlete from './joeathlete.jpg'
-import joeathletemobile from './joeathletemobile.jpg'
-
-import sophiaathlete from './sophiaathlete.jpg'
-import mayaathlete from './mayaathlete.jpg'
-//import cc2 from './cc2.jpg'
-//import UnderConstructionPage from './UnderConstructionPage';
-import natekathlete from './natekathlete.jpg'
-import onomeathlete from './onomeathlete.jpg'
-import addisonathlete from './addisonathlete.jpg' */
-//import OurStory from './OurStory';
 import Disclaimer from './Disclaimer'
 
 
@@ -76,55 +40,6 @@ const useGA4PageTracking = () => {
     };
   }, []);
 };
-
-/* 
-const shots = [
-  nateathlete,
-  katyathlete,
-  oliviathletemobile,
-  jesusathlete,
-  mayaathlete,
-  brookeathlete,
-  joeathletemobile,
-  sophiaathlete,
-  natekathlete,
-  onomeathlete,
-  addisonathlete,
-  ellaathlete,
-  sophiasathlete
-]
-const shots2 = [
-  nateathlete,
-  katyathletedesktop,
-  oliviathlete,
-  jesusathlete,
-  mayaathlete,
-  brookeathlete,
-  joeathlete,
-  sophiaathlete,
-  natekathlete,
-  onomeathlete,
-  addisonathlete,
-  ellaathletedesktop,
-  sophiasathlete
-]
-const texts = [
-  "In my route to Division 1 football, I went through many difficult experiences physically and mentally which I now see as completely avoidable if the proper intervention had been present. In the last few years, learning to apply my neuroscience background to training and daily life has prompted enhanced happiness, health, and success athletically, academically, and otherwise. I want to help others find the same.",
-  "As a Division 1 student-athlete who has experienced many injuries, I have seen firsthand the lack of translation between science and athletics. Cortex Flex gives me a platform and an opportunity to bridge the ever-growing gap between science and athletics, bringing in evidence-based practices that would have helped me throughout my college career.",
-  "Having competed at the NCAA Division I Level for 6 years, and playing competitively for over 10 years before that, my basketball journey consisted of many highs and lows on the court and off. By joining Cortex Flex and bridging the gap between athletes and medical research/science, I hope to promote holistically happy and healthy sports careers for all athletes to come.",
-  "The journey to becoming a Division 1 / Professional athlete is one full of many challenges, over half of them being mental, leading to there being many times throughout the past years I allowed my success as an athlete define what I felt I was worthy of as a human being. My specific role in Cortex Flex will give me a platform to teach young athletes about the ability to care for themselves and create a healthier mindset.",
-  `Drawing from my experience as a Division I tennis player and navigating numerous injuries, I’ve come to understand the immense value of a solid support system, but it was during my medical school journey that I recognized the gap between the medical knowledge a physician has and the practical understanding from the patient’s point of view. As I pursue training during residency in the Physical Medicine and Rehabilitation field, I am dedicated to fostering growth within the Cortex Flex community to help create a space where young athletes can access the insights and guidance they need to fill their potential both on and off the court.`,
-  `As a current Division I soccer player and Integrative Health and Wellness major, I’ve gained a unique perspective to the intersection of sports and holistic well-being. My passion for both soccer and health has driven me to explore ways to optimize performance and promote overall wellness for young athletes and athletes in general. I am deeply interested in joining forces with Cortex Flex to highlight the significance of health in sports and beyond. Through our collaboration, my goal is to grow my experiences and education to advocate for comprehensive health initiatives within the athletic community.`,
-  `As a wrestler at the D1 level, I’ve been through times where I’ve had to “tough it out” whether mentally or physically countless times because that’s what you’re taught to do.  I’ve learned that while mental and physical toughness are imperative to excel, it’s more important to back your trainings by science to improve results and protect your long-term safety.  I’m excited to assist Cortex Flex in being that bridge between Science and Athletics!`,
-  `Living as both a Division 1 athlete and a student of sport science showed me the possibilities that merging these two disciplines can have. My training and well-being benefited immensely from the first-hand knowledge I gained from my degree in exercise science. I want to bring this experience to others through Cortex Flex because I believe it has the power to change lives.`,
-  `As a Division 1 athlete I have gone through my shares of injuries and recognize the lack of continuity between medical research and athletes. I believe in Cortex Flex having the ability to make an impact and bridge the gap between the two sides, and I hope to be able to help athletes handle their injuries better both mentally and physically.`,
-  `As a former Division 1 athlete I went through a pretty tough injury my senior year. No one talks about all the hard work you put in and how it can all end on one play or one lift. Cortex Flex now gives you a platform to talk to other athletes about what you’re going through mentally and physically. I wish I had a group like this when I was going through what I was going through.`,
-  `My athletic experience leading up to and in college exposed me to issues that no one should have to face. Now, I want to share practical knowledge that could have made a world of difference for me – and countless other student-athletes. By bridging the gap between science, athletics, and academics, we can prevent a lot of pain and frustration. While I can't rewind the clock, I'm passionate about empowering future athletes to thrive.`,
-  `As a current Division l volleyball player majoring in health and wellness promotion, I know the importance of balance in all potential aspects of athlete life- ranging from nutrition, time management, recruiting, injury, and athlete’s mental health. My biggest passion is injury and the under-discussed aspect of mental recovery throughout and after the injury process- I am very excited to bring this to Cortex Flex and be a part of what I wish I had access to during my many injuries and their rehabilitation processes.`,
-  `Growing up I was always super interested in the human body and medical field, and was also always in numerous sports year round. Once, I began competing at the Division 1 level for rowing, I truly realized how intertwined these two areas are, and the substantial room for growth in combining sports and medicine. As a result, I joined Cortex to help propel the development of these areas, and to hopefully help other athletes to become the happiest and healthiest they can be.`
-]
- */
-
 
 const theme = createTheme({
   palette: {
@@ -177,40 +92,40 @@ function App() {
   useGA4PageTracking();
   const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
-/*   const images = [
-    graphic1,
-    graphic2,
-    graphic3,
-    graphic4
-    // Add more image paths as needed
-  ]; */
+
   const handleDrawerToggle = () => {
     setMobileMenuOpen(!mobileMenuOpen);
   };
   const drawer = (
     <ThemeProvider theme={theme}>
-      <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
-        <List>
-          <ListItem button component={RouterLink} to="/blog" >
-            <img src={CortexLearn} alt="SABHI Logo" style={{ height: '8px', marginRight: '10px' }} />
-          </ListItem>
-          <ListItem button component={RouterLink} to="/info" >
-            <img src={WHAT} alt="SABHI Logo" style={{ height: '10px', marginTop: '10px' }} />
-          </ListItem>
-          <ListItem button component={RouterLink} to="/research" >
-            <img src={CR} alt="SABHI Logo" style={{ height: '8px', marginTop: '10px'}} />
-          </ListItem>
-          <ListItem button component={RouterLink} to="/pricing">
-            <ListItemText primary="Pricing" />
-          </ListItem> 
-          <ListItem button component={RouterLink} to="/portal">
-            <ListItemText primary="Parent Portal" />
-          </ListItem>
-        </List>
-      </Box>
-    </ThemeProvider>
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+      <List>
+        <ListItem button component={RouterLink} to="/blog">
+          <Button color="inherit" style={{ height: '25px', fontSize: '12.5px', fontFamily: 'Notable, sans-serif' }}>
+            <Typography component="span" style={{ color: '#6589C6', fontSize: '12.5px', fontFamily: 'Notable, sans-serif' }}>CORTEX</Typography>
+            <Typography component="span" style={{ color: '#E75225', fontSize: '12.5px', fontFamily: 'Notable, sans-serif' }}>LEARN</Typography>
+          </Button>
+        </ListItem>
+        <ListItem button component={RouterLink} to="/mentors">
+          <Button color="inherit" style={{ height: '25px', fontSize: '12.5px', fontFamily: 'Notable, sans-serif' }}>
+            <Typography component="span" style={{ color: '#6589C6', fontSize: '12.5px', fontFamily: 'Notable, sans-serif' }}>CORTEX</Typography>
+            <Typography component="span" style={{ color: '#E75225', fontSize: '12.5px', fontFamily: 'Notable, sans-serif' }}>MENTORS</Typography>
+          </Button>
+        </ListItem>
+        <ListItem button component={RouterLink} to="/portal">
+          <Button color="inherit" style={{ height: '25px', fontSize: '12.5px', fontFamily: 'Notable, sans-serif' }}>
+            PARENT PORTAL
+          </Button>
+        </ListItem>
+      </List>
+    </Box>
+  </ThemeProvider>
   );
   return (
+    <div>
+      <style>
+      {`@import url('https://fonts.googleapis.com/css2?family=Notable&display=swap');`}
+      </style>
     <ThemeProvider theme={theme}>
       <Router>
         <Box display="flex" flexDirection="column" minHeight="100vh"> {/* Adjusted for flex layout */}
@@ -253,32 +168,19 @@ function App() {
               ) : (
                 <>
                   <Box display="flex" style={{ marginLeft: '20px' }}> {/* Adjust marginLeft to control spacing */}
-                    <Button color="inherit" component={RouterLink} to="/blog" sx={{
-                      fontWeight: 'bold', // Increases font weight                //animation: 'undulateColor 2s infinite',
-                      // You can adjust the colors and duration as needed
-                    }}>
-                      <img src={CortexLearn} alt="SABHI Logo" style={{ height: '10px', marginRight: '10px' }} />
+                    <Button color="inherit" component={RouterLink} to="/blog"                           style={{ height: '25px', fontSize: '12.5px', fontFamily: 'Notable, sans-serif' }}
+                    >
+                        <Typography component="span" style={{ color: '#6589C6', fontSize: '12.5px', fontFamily: 'Notable, sans-serif' }}>CORTEX</Typography>
+                        <Typography component="span" style={{ color: '#E75225', fontSize: '12.5px', fontFamily: 'Notable, sans-serif' }}>LEARN</Typography>
+                    </Button>
 
+                    <Button color="inherit" component={RouterLink} to="/mentors" style={{ height: '25px', fontSize: '12.5px', fontFamily: 'Notable, sans-serif' }}>
+                        <Typography component="span" style={{ color: '#6589C6', fontSize: '12.5px', fontFamily: 'Notable, sans-serif' }}>CORTEX</Typography>
+                        <Typography component="span" style={{ color: '#E75225', fontSize: '12.5px', fontFamily: 'Notable, sans-serif' }}>MENTORS</Typography>
                     </Button>
-                    <Button color="inherit" component={RouterLink} to="/info">
-                      <img src={WHAT} alt="SABHI Logo" style={{ height: '10px', marginRight: '10px' }} />
-                    </Button>
-                    <Button color="inherit" component={RouterLink} to="/research">
-                      <img src={CR} alt="SABHI Logo" style={{ height: '10px', marginRight: '10px' }} />
-                    </Button>
-                    <Button color="inherit" component={RouterLink} to="/pricing" style={{ height: '25px', fontSize: '12.5px'}}>
-                      PRICING
-                    </Button>
-                    <Button color="inherit" component={RouterLink} to="/portal" style={{ height: '25px', fontSize: '12.5px'}}>
+                    <Button color="inherit" component={RouterLink} to="/portal"   style={{ height: '25px', fontSize: '12.5px', fontFamily: 'Notable, sans-serif' }}                    >
                       PARENT PORTAL
                     </Button>
-                    {/*                     <Button color="inherit" component={RouterLink} to="/contact">
-                      Contact
-                    </Button> 
-                    <Button color="inherit" component={RouterLink} to="/give">
-                      Give
-                    </Button>
-                    */}
                   </Box>
                 </>
               )}
@@ -332,13 +234,15 @@ function App() {
               fontSize: '16px',
             }}
           >
-            Learn More!
+            GET STARTED NOW!
           </Button>
         </a>
       </Box>
+      <Pricing></Pricing>
   </Box>
 ) : 
 ( 
+<div>
   <Box sx={{ position: 'relative', width: '100%', height: '100vh', overflow: 'hidden' }}>
   <video
     autoPlay
@@ -373,11 +277,15 @@ function App() {
   >
     <a href="https://cortexflex.org/info" style={{ textDecoration: 'none' }}>
       <Button variant="contained" color="primary">
-        Learn More!
+      GET STARTED NOW!
       </Button>
     </a>
   </Box>
+  
 </Box> 
+<Pricing></Pricing>
+
+</div>
 )
 
 }       
@@ -414,6 +322,7 @@ function App() {
         </Box>
       </Router>
     </ThemeProvider>
+    </div>
   );
 }
 
--- src/Pricing.js
+++ src/Pricing.js
@@ -1,30 +1,213 @@
-import React from 'react';
-import {PricingTable, PricingSlot, PricingDetail} from 'react-pricing-table';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import Chip from '@mui/material/Chip';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Container from '@mui/material/Container';
+import Divider from '@mui/material/Divider';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import AutoAwesomeIcon from '@mui/icons-material/AutoAwesome';
+import CheckCircleRoundedIcon from '@mui/icons-material/CheckCircleRounded';
 
-class Pricing extends React.Component {
+const tiers = [
+  {
+    title: 'Free',
+    price: '0',
+    description: [
+      '10 users included',
+      '2 GB of storage',
+      'Help center access',
+      'Email support',
+    ],
+    buttonText: 'Sign up for free',
+    buttonVariant: 'outlined',
+  },
+  {
+    title: 'Professional',
+    subheader: 'Recommended',
+    price: '15',
+    description: [
+      '20 users included',
+      '10 GB of storage',
+      'Help center access',
+      'Priority email support',
+      'Dedicated team',
+      'Best deals',
+    ],
+    buttonText: 'Start now',
+    buttonVariant: 'contained',
+  },
+  {
+    title: 'Enterprise',
+    price: '30',
+    description: [
+      '50 users included',
+      '30 GB of storage',
+      'Help center access',
+      'Phone & email support',
+    ],
+    buttonText: 'Contact us',
+    buttonVariant: 'outlined',
+  },
+];
 
-  render() {
-    return  <PricingTable  highlightColor='#1976D2'>
-    <PricingSlot highlighted onClick={() => window.location.href = 'https://cortexflex.copilot.app'} buttonText='GO TO THE PORTAL' title='theCORTEX COMMUNITY' priceText='$20/month*'>
-        <PricingDetail> <b>Access</b> to theCORTEX app and Community</PricingDetail>
-        <PricingDetail> <b>Access</b> to Cortex Courses</PricingDetail>
-        <PricingDetail> <i>*Limited Merit and Need-based Aid Available</i></PricingDetail>
-        </PricingSlot>
-    <PricingSlot  onClick={() => window.location.href = 'https://cortexflex.copilot.app'} buttonText='GO TO THE PORTAL' title='theCORTEX ACADEMY' priceText='$120/month*'>
-        <PricingDetail> <b>Access</b> to theCORTEX App and Community </PricingDetail>
-        <PricingDetail> <b>Access</b> to Cortex Courses</PricingDetail>
-        <PricingDetail> <b>3</b> 30 minute 1:1 meetings with a Community Mentor</PricingDetail>
-        <PricingDetail> <i>*Limited Merit and Need-based Aid Available</i></PricingDetail>
-        </PricingSlot>
-    <PricingSlot    onClick={() => window.location.href = 'cortexflex.copilot.app'} buttonText='GO TO THE PORTAL' title='theCORTEX ACADEMY + 1:1 MENTORSHIP' priceText='Contact the team for more details*'>
-    <PricingDetail> <b>Acess</b> to theCORTEX app and </PricingDetail>
-    <PricingDetail> <b>Access</b> to Cortex Courses</PricingDetail>
-    <PricingDetail> <b>Unlimited</b> 1:1 meetings with a Community Mentor</PricingDetail>
-    <PricingDetail> <i>*Limited Merit and Need-based Aid Available</i></PricingDetail>
-    </PricingSlot>
-</PricingTable>
-
-  }
+export default function Pricing() {
+  return (
+    <Container
+      id="pricing"
+      sx={{
+        pt: { xs: 4, sm: 12 },
+        pb: { xs: 8, sm: 16 },
+        position: 'relative',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        gap: { xs: 3, sm: 6 },
+      }}
+    >
+      <Box
+        sx={{
+          width: { sm: '100%', md: '60%' },
+          textAlign: { sm: 'left', md: 'center' },
+        }}
+      >
+        <Typography component="h2" variant="h4" color="text.primary">
+          Pricing
+        </Typography>
+        <Typography variant="body1" color="text.secondary">
+          Quickly build an effective pricing table for your potential customers with
+          this layout. <br />
+          It&apos;s built with default Material UI components with little
+          customization.
+        </Typography>
+      </Box>
+      <Grid container spacing={3} alignItems="center" justifyContent="center">
+        {tiers.map((tier) => (
+          <Grid
+            item
+            key={tier.title}
+            xs={12}
+            sm={tier.title === 'Enterprise' ? 12 : 6}
+            md={4}
+          >
+            <Card
+              sx={{
+                p: 2,
+                display: 'flex',
+                flexDirection: 'column',
+                gap: 4,
+                border: tier.title === 'Professional' ? '1px solid' : undefined,
+                borderColor:
+                  tier.title === 'Professional' ? 'primary.main' : undefined,
+                background:
+                  tier.title === 'Professional'
+                    ? 'linear-gradient(#033363, #021F3B)'
+                    : undefined,
+              }}
+            >
+              <CardContent>
+                <Box
+                  sx={{
+                    mb: 1,
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
+                    color: tier.title === 'Professional' ? 'grey.100' : '',
+                  }}
+                >
+                  <Typography component="h3" variant="h6">
+                    {tier.title}
+                  </Typography>
+                  {tier.title === 'Professional' && (
+                    <Chip
+                      icon={<AutoAwesomeIcon />}
+                      label={tier.subheader}
+                      size="small"
+                      sx={{
+                        background: (theme) =>
+                          theme.palette.mode === 'light' ? '' : 'none',
+                        backgroundColor: 'primary.contrastText',
+                        '& .MuiChip-label': {
+                          color: 'primary.dark',
+                        },
+                        '& .MuiChip-icon': {
+                          color: 'primary.dark',
+                        },
+                      }}
+                    />
+                  )}
+                </Box>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    alignItems: 'baseline',
+                    color: tier.title === 'Professional' ? 'grey.50' : undefined,
+                  }}
+                >
+                  <Typography component="h3" variant="h2">
+                    ${tier.price}
+                  </Typography>
+                  <Typography component="h3" variant="h6">
+                    &nbsp; per month
+                  </Typography>
+                </Box>
+                <Divider
+                  sx={{
+                    my: 2,
+                    opacity: 0.2,
+                    borderColor: 'grey.500',
+                  }}
+                />
+                {tier.description.map((line) => (
+                  <Box
+                    key={line}
+                    sx={{
+                      py: 1,
+                      display: 'flex',
+                      gap: 1.5,
+                      alignItems: 'center',
+                    }}
+                  >
+                    <CheckCircleRoundedIcon
+                      sx={{
+                        width: 20,
+                        color:
+                          tier.title === 'Professional'
+                            ? 'primary.light'
+                            : 'primary.main',
+                      }}
+                    />
+                    <Typography
+                      component="text"
+                      variant="subtitle2"
+                      sx={{
+                        color:
+                          tier.title === 'Professional' ? 'grey.200' : undefined,
+                      }}
+                    >
+                      {line}
+                    </Typography>
+                  </Box>
+                ))}
+              </CardContent>
+              <CardActions>
+                <Button
+                  fullWidth
+                  variant={tier.buttonVariant}
+                  component="a"
+                  href="/material-ui/getting-started/templates/checkout/"
+                  target="_blank"
+                >
+                  {tier.buttonText}
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+  );
 }
-
-export default Pricing;
